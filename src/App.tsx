892af6c089b6cc2e254de010fd710ddbe43a7f3d
Lin Yi-Hsuan <98067377+AubreyLin0@users.noreply.github.com>
2024-03-03 16:13:57 +0900

    add new component - option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { SimpleGrid } from "@chakra-ui/react";
 import AddOptionsForm from "./components/AddOptionsForm";
+import Option from "./components/Option";
 
 function App() {
   return (
     <div className="border h-screen w-full">
       <h5 className="font-madimi-one text-3xl">Option Picker</h5>
       <AddOptionsForm />
+      <SimpleGrid
+        columns={3}
+        spacing={5}
+        width={"50vw"}
+        className="border mx-auto"
+      >
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+      </SimpleGrid>
     </div>
   );
 }
---
+++ src/components/Option.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody, CardHeader, Divider } from "@chakra-ui/react";
+
+const Option = () => {
+  return (
+    <Card>
+      <CardHeader>Test Head</CardHeader>
+      <Divider color={"gray.300"} />
+      <CardBody>Test Body</CardBody>
+    </Card>
+  );
+};
+
+export default Option;
